e7961c677228fdc2375451ed109f8ac5cb829dcc
ABC2gameYT <69885556+ABC2gameYT@users.noreply.github.com>
2024-03-28 19:36:09 -0400

    First

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.get("/getmoderation", function(req, res) {
+app.get("/getanalytics", function(req, res) {
     res.json({})
 });
 
