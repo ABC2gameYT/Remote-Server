c732a8b7b688dd6fc0314eebb53e0825c2b12385
ABC2gameYT <69885556+ABC2gameYT@users.noreply.github.com>
2024-03-28 19:43:44 -0400

    Adding to api

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get("/getanalytics", function(req, res) {
     res.json({})
 });
 
+app.get("/getanalytics2", function(req, res) {
+    res.json({a: b})
+});
+
 const listener = app.listen(process.env.PORT, () => {
     console.log("Your app is listening on port " + listener.address().port);
 });
