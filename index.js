3f02a5baa51bc94860f41c5d1d3c69faa128d8cd
ABC2gameYT <69885556+ABC2gameYT@users.noreply.github.com>
2024-03-28 20:05:21 -0400

    Added to analytics api

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const app = express();
+const admin = require("firebase-admin");
+
+admin.initializeApp({
+    credential: admin.credential.cert(require("dotenv").config.env.firebase),
+    databaseURL: "https://analytics-d82d2-default-rtdb.firebaseio.com",
+});
+
+ const { getDatabase } = require("firebase-admin/database");
+ const db = getDatabase();
+ const ref = db.ref("Analytics")
 
 app.use(express.json());
 app.get("/getanalytics", function(req, res) {
-    res.json({})
-});
+    ref.get().then((snapshot => {
+        snapshot = snapshot.val()
 
-app.get("/getanalytics2", function(req, res) {
-    res.json({a: b})
+        res.json(snapshot);
+    }));
 });
 
 const listener = app.listen(process.env.PORT, () => {
